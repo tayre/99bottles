2c713ca4e76eaf61be2ff9e82eb6b643304b48a2
Tom Ayre <tom.ayre@shopify.com>
2019-02-09 21:23:13 -0500

    1.1.1. Incomprehensible Concise

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,71 +1,19 @@
-# frozen_string_literal: true
-
-# 99 problems exercise
 class Bottles
-
   def song
     verses(99, 0)
   end
 
-  def verses(start_verse, end_verse)
-    result = []
-    current_verse = start_verse
-
-    while current_verse >= end_verse
-      result << verse(current_verse)
-      current_verse -= 1
-    end
-    result.join("\n")
+  def verses(hi, lo)
+    hi.downto(lo).map {|n| verse(n) }.join("\n")
   end
 
-  def verse(verse_number)
-    case verse_number
-    when 0
-      zero_bottle_verse
-    when 1
-      one_bottle_verse
-    when 2
-      two_bottle_verse
-    else
-      default_verse(verse_number)
-    end
-  end
-
-  private
-
-  def default_verse(verse_number)
-    [
-      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.",
-      "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall."
-    ]
-    .map { |verse| "#{verse}\n" }
-    .join
-  end
-
-  def two_bottle_verse
-    [
-      "2 bottles of beer on the wall, 2 bottles of beer.",
-      "Take one down and pass it around, 1 bottle of beer on the wall."
-    ]
-    .map { |verse| "#{verse}\n" }
-    .join
-  end
-
-  def one_bottle_verse
-    [
-      "1 bottle of beer on the wall, 1 bottle of beer.",
-      "Take it down and pass it around, no more bottles of beer on the wall."
-    ]
-    .map { |verse| "#{verse}\n" }
-    .join
-  end
-
-  def zero_bottle_verse
-    [
-      "No more bottles of beer on the wall, no more bottles of beer.",
-      "Go to the store and buy some more, 99 bottles of beer on the wall."
-    ]
-    .map { |verse| "#{verse}\n" }
-    .join
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+    " of beer on the wall, " +
+    "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+    "#{n > 0  ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+              : "Go to the store and buy some more"}, " +
+    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+    " of beer on the wall.\n"
   end
 end
