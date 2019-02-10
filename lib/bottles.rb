c2c6d7e7dfb55f235db9b2f31a36891033a1db9e
Tom Ayre <tom.ayre@shopify.com>
2019-02-09 21:57:30 -0500

    Second pass

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,88 +1,50 @@
+# frozen_string_literal: true
+
+# 99 problems exercise
 class Bottles
 
   def song
     verses(99, 0)
   end
 
-  def verses(bottles_at_start, bottles_at_end)
-    bottles_at_start.downto(bottles_at_end).map do |bottles|
-      verse(bottles)
+  def verses(start_verse, end_verse)
+    start_verse.downto(end_verse).map do |verse_number|
+      verse(verse_number)
     end.join("\n")
   end
 
-  def verse(bottles)
-    Round.new(bottles).to_s
-  end
-end
-
-class Round
-  attr_reader :bottles
-  def initialize(bottles)
-    @bottles = bottles
-  end
-
-  def to_s
-    challenge + response
-  end
-
-  def challenge
-    bottles_of_beer.capitalize + " " + on_wall + ", " +
-    bottles_of_beer + ".\n"
-  end
-
-  def response
-    go_to_the_store_or_take_one_down + ", " +
-    bottles_of_beer + " " + on_wall + ".\n"
-  end
-
-  def bottles_of_beer
-    "#{anglicized_bottle_count} #{pluralized_bottle_form} of #{beer}"
-  end
-
-  def beer
-    "beer"
-  end
-
-  def on_wall
-    "on the wall"
-  end
-
-  def pluralized_bottle_form
-    last_beer? ? "bottle" : "bottles"
-  end
-
-  def anglicized_bottle_count
-    all_out? ? "no more" : bottles.to_s
-  end
-
-  def go_to_the_store_or_take_one_down
-    if all_out?
-      @bottles = 99
-      buy_new_beer
+  def verse(verse_number)
+    case verse_number
+    when 0
+      zero_bottle_verse
+    when 1
+      one_bottle_verse
+    when 2
+      two_bottle_verse
     else
-      lyrics = drink_beer
-      @bottles -= 1
-      lyrics
+      default_verse(verse_number)
     end
   end
 
-  def buy_new_beer
-    "Go to the store and buy some more"
+  private
+
+  def default_verse(verse_number)
+    "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n"+
+    "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
   end
 
-  def drink_beer
-    "Take #{it_or_one} down and pass it around"
+  def two_bottle_verse
+    "2 bottles of beer on the wall, 2 bottles of beer.\n"+
+    "Take one down and pass it around, 1 bottle of beer on the wall.\n"
   end
 
-  def it_or_one
-    last_beer? ? "it" : "one"
+  def one_bottle_verse
+    "1 bottle of beer on the wall, 1 bottle of beer.\n"+
+    "Take it down and pass it around, no more bottles of beer on the wall.\n"
   end
 
-  def all_out?
-    bottles.zero?
-  end
-
-  def last_beer?
-    bottles == 1
+  def zero_bottle_verse
+    "No more bottles of beer on the wall, no more bottles of beer.\n"+
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 end
